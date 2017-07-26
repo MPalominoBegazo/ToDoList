0a93663873eb2dda0c97528afcbfe7a506979b86
Maria del Rosario Palomino Begazo <maria_rosario789@hotmail.com>
2017-07-25 23:59:29 -0500

    ProyectoSprint2

--- js/main.js
+++ js/main.js
@@ -1,107 +1,132 @@
-var arrDatos = [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 10,
-    "title": "illo est ratione doloremque quia maiores aut",
-    "completed": true
+
+function Tareas(title) {
+  this.userId = 1
+  this.title = title;
+  this.completed = false;
+
+}
+
+function ToDoList() {
+  this.arrDatos = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "title": "illo expedita consequatur quia in",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 8,
+      "title": "quo adipisci enim quam ut ab",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 9,
+      "title": "molestiae perspiciatis ipsa",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 10,
+      "title": "illo est ratione doloremque quia maiores aut",
+      "completed": false
+    }
+  ];
+  this.add = function (tareas) {
+    this.arrDatos.push(tareas);
   }
-];
+  this.isChecked = function (event) {
+    if (event.target.tagName === 'LI') {
+      event.target.classList.toggle('checked');
+    }
+
+  }
+  this.mostrar = function (content) {
+    var newLi = document.createElement('li');
+    var checkBox = cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.id = "c1";
+    cb.value = content;
+    cb.checked = false;
+
+    newLi.appendChild(cb);
+
+    var text = document.createTextNode(content);
+    newLi.appendChild(text);
+
+    var ul = document.getElementById("lista");
+    ul.appendChild(newLi).contentEditable = true;
+
+
+  }
+}
+
+var list = new ToDoList();
+
 
 listar();
-function Tareas(title){
-    this.userId =1;
-   // this.id = undefined;
-    this.title = title;
-    this.completed = false;
-    /*this.isChecked = function(){
-        resul.innerHTML =' <p><input type="checkbox" id="'+i+'"><label for="'+i+'" editable>'+datos.title+'</label></p>';
-        
-    }*/
+listas();
+var btnAdd = document.getElementById("btnActivity");
+btnAdd.onclick = function add() {
+  var content = document.getElementById("Activity").value;
+  list.mostrar(content);
+  list.add(new Tareas(content));
+  listas();
+  return false;
 }
 
-var buttonAdd = document.getElementById("buttonAdd");
-buttonAdd.onclick = function (){
-    var activity = document.getElementById("addActivity").value;
-    var tasks = new Tareas(activity);
-    arrDatos.push(tasks);
-  //  console.log(arrDatos);
-   limpiar();
-   listar();
-   return false;
-    
+
+function listar() {
+  for (var i in list.arrDatos) {
+    datos = list.arrDatos[i];
+    list.mostrar(datos.title);
+  }
 }
 
-function listar(){
-    for(var i in arrDatos){
-        datos = arrDatos[i];
-        var resul = document.getElementById("resultado");
-         resul.innerHTML +=' <p><input type="checkbox" id="'+i+'"><label for="'+i+'" editable>'+datos.title+'</label></p>';
-      /*  if(i.checked){
-           
-            datos.completed = true;
 
-        }*/
+function listas() {
+  var listas = document.getElementsByTagName("li");
+  for (var i in listas) {
+    listas[i].onclick = function (event) {
+      list.isChecked(event);
     }
-    console.log(arrDatos);
+    console.log(listas);
+  }
 }
-
-function limpiar(){
-    document.getElementById("resultado").innerHTML="";
-    document.getElementById("addActivity").value="";
-}
-/*<input type="checkbox" id="filled-in-box" checked="checked" />
-      <label for="filled-in-box">Filled in</label> */
